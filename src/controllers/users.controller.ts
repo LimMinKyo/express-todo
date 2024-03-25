1e8f9a202104b83075b193d72e323110edc13f41
LimMinKyo <story1400@naver.com>
2024-03-25 16:47:31 +0900

    Update: users API

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,19 +1,20 @@
 import { Route, Tags, OperationId, Get, Security, Request } from "tsoa";
 import { GetMyInfoResponse } from "../dtos/users/my.dto";
 import express from "express";
+import Container from "typedi";
+import UsersService from "../services/users.service";
 
 const JWT_KEY = "jwt";
 
 @Tags("유저API")
 @Route("api/users")
 export default class UsersController {
+  private readonly usersService = Container.get(UsersService);
+
   @Get("/my")
   @OperationId("내정보")
   @Security(JWT_KEY)
   async getMyInfo(@Request() req: express.Request): Promise<GetMyInfoResponse> {
-    return {
-      ok: true,
-      data: req.user,
-    };
+    return await this.usersService.getMyInfo(req.user!);
   }
 }
--- src/services/users.service.ts
+++ src/services/users.service.ts
@@ -6,6 +6,15 @@ import User from "../entities/user.entity";
 export default class UsersService {
   private readonly usersRepository = AppDataSource.getRepository(User);
 
+  async getMyInfo(user: User) {
+    const { password, ...rest } = user;
+
+    return {
+      ok: true,
+      data: rest,
+    };
+  }
+
   async findById(userId: number): Promise<User | null> {
     const user = await this.usersRepository.findOne({
       where: { id: userId },
